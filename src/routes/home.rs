ccf2f0ad56016246b51ec835c0093ce8df7f2d86
philidaquin <philipdaquin20@gmail.com>
2022-01-28 17:55:35 +0800

    Button Add To Car

--- src/routes/home.rs
+++ src/routes/home.rs
@@ -3,6 +3,7 @@ use yew::prelude::*;
 /// Designing the State of this component
 /// - We need to store a list of products retrieved from server
 /// - Store the products the user has added to cart
+#[derive(Clone)]
 pub struct Product { 
     id: i32,
     name: String,
@@ -10,17 +11,30 @@ pub struct Product {
     image: String,
     price: f64
 }
-
-struct State { 
-    products: Vec<Product>
+/// AddToCart component: 
+/// - Track all products added to cart in a new state called 'cart_products'
+/// - Render a 'add_to_cart' button for each product
+/// - Add logic to update the 'cart_products' state when 'add_to_cart' button is clicked 
+pub struct AddToCart  {
+    product: Product,
+    quantity: i32
 }
-
+struct State { 
+    products: Vec<Product>,
+    cart_products: Vec<AddToCart>
+}
+pub enum Msg { 
+    AddToCart(i32)
+}
+/// ComponentLinks: Help us create CallBack functions which allows us to change a piece of the state
+/// that is part of the parent component. it a Child-to-Parent communication
 pub struct Home { 
-    state: State
+    state: State,
+    _link: ComponentLink<Self>
 }
 
 impl Component for Home {
-    type Message = ();
+    type Message = Msg;
     type Properties = ();
 
     fn create(_props: Self::Properties, _link: ComponentLink<Self>) -> Self {
@@ -34,16 +48,46 @@ impl Component for Home {
             }
 
         ];
+        let cart_products: Vec<AddToCart> = vec![];
         
         Self { 
             state: State { 
-                products
-            }
+                products,
+                cart_products
+            },
+            _link
         }
     }
-
+    ///  The communication between components happens primarly through Messages 
+    ///  This allows the component to update itself based on what the based messaged was and rerednered 
+    ///  itself
     fn update(&mut self, _msg: Self::Message) -> ShouldRender {
-        true
+        match _msg { 
+            //  Find the product_id from the product list
+            Msg::AddToCart(id) => { 
+                let product = self
+                    .state
+                    .products
+                    .iter()
+                    .find(|item| item.id==id)
+                    .unwrap();
+                let cart = self
+                    .state
+                    .cart_products
+                    .iter_mut()
+                    .find(|prod| prod.product.id == id);
+
+                if let Some(prod) = cart { 
+                    prod.quantity +=1;
+                } else { 
+                    self.state.cart_products.push(AddToCart { 
+                        product: product.clone(),
+                        quantity: 1
+                    })
+                }
+                true
+            },
+        }
     }
 
     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
@@ -53,14 +97,22 @@ impl Component for Home {
     fn view(&self) -> Html {
 
         let products: Vec<_> = self.state.products.iter().map(|product| { 
+            let id = product.id;
             html! {
                 <div>
                     <img src={product.image.clone()}/>
                     <div>{product.name.clone()}</div>
+                    <div>{"$"}{product.price}</div>
+                    <button onclick=self._link.callback(move |_| Msg::AddToCart(id))>{"Add to Cart"}</button>
                 </div>
             }
         }).collect();
 
+        let cart = self
+            .state
+            .cart_products
+            .iter()
+            .fold(0.0, |total, cart| total + (cart.quantity as f64 * cart.product.price));
 
 
         html! {
@@ -68,6 +120,8 @@ impl Component for Home {
                 <img class="profile-picture" src="assets/images/avatar.jpg" alt="ShironCat's avatar" />
                 <h1>{ "Hello, World!" }</h1>
                 <span>{products}</span>
+                <span>{format!("Cart Value: {:.2}", cart)}</span>
+
             </div>
         }
     }
